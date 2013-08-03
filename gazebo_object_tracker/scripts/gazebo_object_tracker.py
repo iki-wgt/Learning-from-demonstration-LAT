8ad7cac981c006eed592f93ac130f72f3d0fcff7
Benny <reinerbe@hs-weingarten.de>
2013-08-03 18:19:11 +0200

    Using better method to get model positions

--- gazebo_object_tracker/scripts/gazebo_object_tracker.py
+++ gazebo_object_tracker/scripts/gazebo_object_tracker.py
@@ -2,7 +2,7 @@
 import roslib; roslib.load_manifest('gazebo_object_tracker')
 import rospy
 from geometry_msgs.msg import Pose
-from gazebo.srv import GetWorldProperties, GetModelState
+from gazebo_msgs.msg import ModelStates, ModelState
 from ar_pose.msg import ARMarkers, ARMarker
 from ar_track_alvar.msg import AlvarMarkers, AlvarMarker
 
@@ -19,32 +19,17 @@ class GazeboObjectTracker:
     def __init__(self):
         rospy.init_node('gazebo_object_tracker')
         
-        self.objs = None
+        self.objs = {}
         self.objDict = {'COCA-COLA-CAN-250ML' : 1, 'IKEA-CUP-SOLBRAEND-BLUE' : 2}
         
-        rospy.wait_for_service('/gazebo/get_world_properties')
-        rospy.wait_for_service('/gazebo/get_model_state')
-        
-        self.gazebo_world_prop = rospy.ServiceProxy('/gazebo/get_world_properties', GetWorldProperties, persistent=True)
-        self.gazebo_model_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState, persistent=True)
+        rospy.Subscriber("/gazebo/model_states", ModelStates, self.callback)
         
         rospy.loginfo("gazebo object tracker started")
-        
-    def load_id_and_pose_from_gazebo(self):
-      # init our members
-      self.objs = {}
-      # find objects in the world
-      world = self.gazebo_world_prop()
-
-      for obj in world.model_names:
-        model_state = self.gazebo_model_state(model_name=obj)
-        self.objs[str(obj)] = model_state.pose
 
     def publish(self):
         pub = rospy.Publisher('ar_pose_marker', ARMarkers)
         seq = 0
         while not rospy.is_shutdown():
-            self.load_id_and_pose_from_gazebo()
             markers = ARMarkers()
             markers.header.stamp.secs = rospy.get_time()
             markers.header.seq = seq
@@ -64,8 +49,13 @@ class GazeboObjectTracker:
             pub.publish(markers)
             seq = seq + 1
             
+            # no spinning needed in rospy
             rospy.sleep(1 / PUB_FREQ)
-            
+     
+    def callback(self, data):
+         for objIdx in range(len(data.name)):
+             self.objs[str(data.name[objIdx])] = data.pose[objIdx]
+     
 if __name__ == '__main__':
     tracker = GazeboObjectTracker()
     try:
