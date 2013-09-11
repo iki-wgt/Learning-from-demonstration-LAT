8c2278a5eeb2cf4ed08a975d4997de526807f842
Benny <reinerbe@hs-weingarten.de>
2013-09-11 14:56:10 +0200

    Implemented test for group constraint

--- ros_lfd_lat/test/ndmaptest.cpp
+++ ros_lfd_lat/test/ndmaptest.cpp
@@ -3,11 +3,11 @@
 // Bring in gtest
 #include <gtest/gtest.h>
 
-ndmap createNdmap()
+ndmap createNdmap1()
 {
-	// values from the coke
+	// values from the cup
 
-	double dim1[] = {0.03, 0.05, 0.025, 0.01, 0.01, 0.009, 0.025, 0.025, 0.1, 0.11, 0.11, 0.07, 0.05, 0.05};	// values from the cup
+	double dim1[] = {0.1, 0.125, 0.1, 0.1, 0.09, 0.09, 0.08, 0.05, 0.03, 0.045, 0.045, 0.03, 0.11, 0.12};
 	double dim2[] = {0.22, 0.4, 0.5, 0.6, 0.6, 0.6, 0.5, 0.11, 0.015, 0.015, 0.015, 0.018, 0.38, 0.35};
 	double dim3[] = {0.03, 0.02, 0.07, 0.03, 0.04, 0.04, 0.02, 0.05, 0.01, 0.01, 0.01, 0.06, 0.03, 0.07};
 
@@ -29,11 +29,37 @@ ndmap createNdmap()
 	return ndm;
 }
 
-ndmapSet createNdmapSet(std::string name)
+ndmap createNdmap2()
+{
+	// values from the coke
+
+	double dim1[] = {0.03, 0.05, 0.025, 0.01, 0.01, 0.009, 0.025, 0.025, 0.1, 0.11, 0.11, 0.07, 0.05, 0.05};
+	double dim2[] = {0.3, 0.2, 0.09, 0.01, 0.01, 0.015, 0.07, 0.48, 0.6, 0.6, 0.6, 0.58, 0.22, 0.23};
+	double dim3[] = {0.07, 0.05, 0.04, 0.02, 0.02, 0.025, 0.02, 0.025, 0.025, 0.04, 0.04, 0.04, 0.03, 0.06};
+
+	ndmap ndm;
+
+	ndm.add_deque(std::deque<double>());
+	ndm.add_deque(std::deque<double>());
+	ndm.add_deque(std::deque<double>());
+
+	for (int i = 0; i < 14; ++i)
+	{
+		std::deque<double> point;
+		point.push_back(dim1[i]);
+		point.push_back(dim2[i]);
+		point.push_back(dim3[i]);
+		ndm.push_back(point);
+	}
+
+	return ndm;
+}
+
+ndmapSet createNdmapSet1(std::string name)
 {
 	ndmapSet ndmSet;
 	ndmSet.set_name(name);
-	ndmap ndm = createNdmap();
+	ndmap ndm = createNdmap1();
 	ndm.set_name(ndmSet.get_name() + "_mean+stdev");
 	ndmSet.add_ndmap(ndm);
 	ndm.set_name(ndmSet.get_name() + "_mean");
@@ -46,9 +72,36 @@ ndmapSet createNdmapSet(std::string name)
 	return ndmSet;
 }
 
+ndmapSet createNdmapSet2(std::string name)
+{
+	ndmapSet ndmSet;
+	ndmSet.set_name(name);
+	ndmap ndm = createNdmap2();
+	ndm.set_name(ndmSet.get_name() + "_mean+stdev");
+	ndmSet.add_ndmap(ndm);
+	ndm.set_name(ndmSet.get_name() + "_mean");
+	ndmSet.add_ndmap(ndm);
+	ndm.set_name(ndmSet.get_name() + "_mean-stdev");
+	ndmSet.add_ndmap(ndm);
+	ndm.set_name(ndmSet.get_name() + "_stdev");
+	ndmSet.add_ndmap(ndm);
+
+	return ndmSet;
+}
+
+ndmapSetGroup createNdmapSetGroup()
+{
+	ndmapSetGroup group = ndmapSetGroup();
+
+	group.add_ndmapSet(createNdmapSet1("cup"));
+	group.add_ndmapSet(createNdmapSet1("coke"));
+
+	return group;
+}
+
 TEST(TestSuite, testGetMinMax)
 {
-	ndmap ndm = createNdmap();
+	ndmap ndm = createNdmap1();
 
 	int dim = ndm.get_dim();
 	ASSERT_EQ(3, dim) << "Dimension of ndmap not equal 3";
@@ -59,8 +112,8 @@ TEST(TestSuite, testGetMinMax)
 	try
 	{
 		ndm.getMinMax(0, min, max);
-		EXPECT_DOUBLE_EQ(0.009, min) << "Min value of row 0 not  equal 0.009";
-		EXPECT_DOUBLE_EQ(0.11, max) << "Max value of row 0 not  equal 0.11";
+		EXPECT_DOUBLE_EQ(0.03, min) << "Min value of row 0 not  equal 0.03";
+		EXPECT_DOUBLE_EQ(0.125, max) << "Max value of row 0 not  equal 0.125";
 
 		ndm.getMinMax(1, min, max);
 		EXPECT_DOUBLE_EQ(0.015, min) << "Min value of row 1 not  equal 0.015";
@@ -79,7 +132,7 @@ TEST(TestSuite, testGetMinMax)
 
 TEST(TestSuite, testGetDimWithMaxDev)
 {
-	ndmap ndm = createNdmap();
+	ndmap ndm = createNdmap1();
 
 	unsigned int dimWithMaxDev = ndm.getDimWithMaxDev();
 	EXPECT_EQ(1, dimWithMaxDev) << "Returned dim not the one with max dev";
@@ -87,7 +140,7 @@ TEST(TestSuite, testGetDimWithMaxDev)
 
 TEST(TestSuite, testGetConstraints)
 {
-	ndmapSet ndmSet = createNdmapSet("object1");
+	ndmapSet ndmSet = createNdmapSet1("object1");
 	double threshold1 = 0.015;
 	double threshold2 = 0.11;
 	bool resultArray1[] =
@@ -113,43 +166,33 @@ TEST(TestSuite, testGetConstraints)
 	ASSERT_EQ(expectedResult2.size(), result2.size()) << "Size of result 2 incorrect";
 
 	for (unsigned int i = 0; i < expectedResult1.size(); ++i) {
-		ASSERT_EQ(expectedResult1[i], result1[i]) << "Result 1 incorrect";
-		ASSERT_EQ(expectedResult2[i], result2[i]) << "Result 2 incorrect";
+		EXPECT_EQ(expectedResult1[i], result1[i]) << "Result 1 incorrect";
+		EXPECT_EQ(expectedResult2[i], result2[i]) << "Result 2 incorrect";
 	}
 }
 
-/*TEST(TestSuite, testGetConstraintsGroup)
+TEST(TestSuite, testGetConstraintsGroup)
 {
-	ndmapSet ndmSet = createNdmapSet("object1");
-	double threshold1 = 1.2;
-	double threshold2 = 1.3;
-	bool resultArray1[] =
-		{true, true, true, false, false, false, false, false, false, false, false, false, false, false, false};
-	bool resultArray2[] =
-		{true, true, true, false, false, false, false, false, false, false, false, true, true, false, false};
+	ndmapSetGroup group = createNdmapSetGroup();
 
-	std::deque<bool> expectedResult1 = std::deque<bool>();
-	std::deque<bool> expectedResult2 = std::deque<bool>();
+	bool resultArray[] =
+		{false, false, false, true, true, true, false, false, true, true, true, true, false, false};
+
+	std::deque<bool> expectedResult = std::deque<bool>();
 
 	for (int i = 0; i < 15; ++i)
 	{
-		expectedResult1.push_back(resultArray1[i]);
-		expectedResult2.push_back(resultArray2[i]);
+		expectedResult.push_back(resultArray[i]);
 	}
 
-	std::deque<bool> result1 = ndmSet.getConstraints(threshold1);
+	std::deque<bool> result = group.getConstraints();
 
-	ASSERT_EQ(expectedResult1.size(), result1.size()) << "Size of result 1 incorrect";
+	ASSERT_EQ(expectedResult.size(), result.size()) << "Size of the result incorrect";
 
-	std::deque<bool> result2 = ndmSet.getConstraints(threshold2);
-
-	ASSERT_EQ(expectedResult2.size(), result2.size()) << "Size of result 2 incorrect";
-
-	for (unsigned int i = 0; i < expectedResult1.size(); ++i) {
-		ASSERT_EQ(expectedResult1[i], result1[i]) << "Result 1 incorrect";
-		ASSERT_EQ(expectedResult2[i], result2[i]) << "Result 2 incorrect";
+	for (unsigned int i = 0; i < expectedResult.size(); ++i) {
+		EXPECT_EQ(expectedResult[i], result[i]) << "Result incorrect";
 	}
-}*/
+}
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
